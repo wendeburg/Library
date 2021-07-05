438fbb9f2cc781c2fcb9fe7445861f6c40ebe16f
fwendeburg <86864927+fwendeburg@users.noreply.github.com>
2021-07-06 00:17:14 +0200

    Changed book object attributes and added more functionalities to modal.
    Added book or exiting the modal clears the form. Added publish date asattribute to the book object.

--- javascript/main.js
+++ javascript/main.js
@@ -7,18 +7,18 @@ const bookTitleInput = document.querySelector('#book-title');
 const bookCoverImage = document.querySelector('#book-cover');
 const bookAuthorInput = document.querySelector('#book-author');
 const bookPagesInput = document.querySelector('#book-pages');
-const bookReadPagesInput = document.querySelector('#book-pages-read');
+const bookPublishDateInput = document.querySelector('#book-publish-date');
 const bookReadStatus = document.querySelector('#book-read');
 
 
 let myLibrary = [];
 
-function Book(title, URL, author, totalPages, readPages, isRead) {
+function Book(title, URL, author, totalPages, publishDate, isRead) {
     this.title = title;
     this.coverImage = URL;
     this.author = author;
     this.totalPages = totalPages;
-    this.readPages = readPages;
+    this.publishDate = publishDate;
     this.isRead = isRead;
 
     this.info = function() {
@@ -31,17 +31,17 @@ function addBookToLibrary() {
     let bookImage = bookCoverImage.value;
     let bookAuthor = bookAuthorInput.value;
     let bookPages = bookPagesInput.value;
-    let bookReadPages = bookReadPagesInput.value;
+    let bookPublishDate = setPublishDate(bookPublishDateInput.value);
     let bookIsRead = bookReadStatus.checked;
 
+    modal.classList.toggle('hide');
+    clearInputs();
 
-    let newBook = new Book(bookTitle, bookImage, bookAuthor, bookPages, bookReadPages, bookIsRead);
+    let newBook = new Book(bookTitle, bookImage, bookAuthor, bookPages, bookPublishDate, bookIsRead);
 
     myLibrary.push(newBook);
 
     createBookEntry(newBook);
-
-    modal.classList.toggle('hide');
 }
 
 function removeBook(e) {
@@ -51,45 +51,51 @@ function removeBook(e) {
     book.remove();
 }
 
+function setPublishDate(date) {
+    let dateElements = date.split("-");
+
+    return `Published on: ${dateElements[2]}/${dateElements[1]}/${dateElements[0]}`;
+}
+
 function createBookEntry(book) {
-    bookCard = document.createElement('div');
+    let bookCard = document.createElement('div');
     bookCard.setAttribute("id", `${myLibrary.length-1}`);
     bookCard.classList.add('book');
 
-    bookInfo = document.createElement('div');
+    let bookInfo = document.createElement('div');
     bookInfo.classList.add('book-info');
 
-    bookTitle = document.createElement('h2');
+    let bookTitle = document.createElement('h2');
     bookTitle.classList.add('book-title');
-    bookTitle.innerHTML = `${book.title}`; //max 29 chars
+    bookTitle.textContent = book.title;
 
-    bookAuthor = document.createElement('p');
+    let bookAuthor = document.createElement('p');
     bookAuthor.classList.add('book-author');
-    bookAuthor.innerHTML = book.author;
+    bookAuthor.textContent = book.author;
 
-    bookPages = document.createElement('p');
+    let bookPages = document.createElement('p');
     bookPages.classList.add('book-pages');
-    bookPages.innerHTML = book.totalPages;
+    bookPages.textContent = `${book.totalPages} pages`;
 
-    bookReadPages = document.createElement('p');
-    bookReadPages.classList.add('read-pages');
-    bookReadPages.innerHTML = book.readPages;
+    let bookPublishDate = document.createElement('p');
+    bookPublishDate.classList.add('publish-date');
+    bookPublishDate.textContent = book.publishDate;
 
-    bookReadBtn = document.createElement('button');
+    let bookReadBtn = document.createElement('button');
     bookReadBtn.setAttribute('id', 'read-book');
-    bookReadBtn.innerHTML = 'Not Read';
+    bookReadBtn.textContent = 'Not Read';
 
-    bookRemoveBtn = document.createElement('button');
+    let bookRemoveBtn = document.createElement('button');
     bookRemoveBtn.setAttribute("data-index", `${myLibrary.length-1}`);
     bookRemoveBtn.setAttribute('id', 'remove-book');
-    bookRemoveBtn.innerHTML = 'Remove';
+    bookRemoveBtn.textContent = 'Remove Book';
 
     bookRemoveBtn.addEventListener('click', removeBook)
 
     bookInfo.append(bookTitle);
     bookInfo.append(bookAuthor);
     bookInfo.append(bookPages);
-    bookInfo.append(bookReadPages);
+    bookInfo.append(bookPublishDate);
     bookInfo.append(bookReadBtn);
     bookInfo.append(bookRemoveBtn);
     bookCard.append(bookInfo);
@@ -99,11 +105,22 @@ function createBookEntry(book) {
 function toggleModal(e) {
     modal.classList.toggle('hide');
 
-    if (e.target.classList.contains("show-modal")) {
+    if (e.target.classList.contains('show-modal')) {
         menuOptions.classList.toggle('show-menu');
         burgerMenuBtn.classList.toggle('open');
         body.classList.toggle('hidden-overflow');
     }
+    else if (e.target.id === 'cancel-btn') {
+        clearInputs();
+    }
+}
+
+function clearInputs() {
+bookTitleInput.value = '';
+bookCoverImage.value = '';
+bookAuthorInput.value = '';
+bookPagesInput.value = '';
+bookPublishDateInput.value = '';
 }
 
 addBookBtn.addEventListener('click', addBookToLibrary);
